a671552a2c5faf22782926a9a2506ae23ec1c5d5
卫慧杰 <w920098695@sina.cn>
2022-07-10 01:26:57 +0800

    feat: 封装路由

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,12 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 
-const userRouter = require('./../router/user.router')
-const authRouter = require('./../router/auth.router')
+const useRoutes = require('./../router')
+
 const errorHandler = require('./error-handler')
 const app = new Koa()
 
 app.use(bodyParser())
-
-app.use(userRouter.routes())
-app.use(userRouter.allowedMethods())
-app.use(authRouter.routes())
-app.use(authRouter.allowedMethods())
-
+useRoutes(app)
 app.on('error', errorHandler)
 module.exports = app
