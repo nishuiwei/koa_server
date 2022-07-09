912a2d9e3fc754a5fd38d7423034aec44ea81595
卫慧杰 <w920098695@sina.cn>
2022-07-10 01:18:34 +0800

    feat: 登录验证

--- src/app/error-handler.js
+++ src/app/error-handler.js
@@ -5,12 +5,21 @@ const errorHandler = (error, ctx) => {
 	switch (error.message) {
 		case errorType.NAME_OR_PASSWORD_IS_REQUIRED:
 			status = 400
-			message = '用户名或者密码不能为空'
+			message = '用户名或者密码不能为空-'
 			break
 
 		case errorType.USER_ALREADY_EXISTS:
 			status = 409
-			message = '用户名已存在'
+			message = '用户名已存在-'
+			break
+
+		case errorType.USER_DOES_NOT_EXISTS:
+			status = 400
+			message = '用户名不存在-'
+			break
+		case errorType.PASSWORD_IS_INCORRENT:
+			status = 400
+			message = '密码错误-'
 			break
 
 		default:
---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+const errorType = require('../constants/error-types')
+const { getUserByName } = require('../service/user.service')
+const md5password = require('../utils/password-handle')
+
+const verifyLogin = async (ctx, next) => {
+	// 1. 获取用户名和密码
+	const { name, password } = ctx.request.body
+	// 2. 判断用户名和密码是否为空
+	if (!name.trim() || !password.trim()) {
+		const error = new Error(errorType.NAME_OR_PASSWORD_IS_REQUIRED)
+		return ctx.app.emit('error', error, ctx)
+	}
+	// 3. 判断用户是否存在
+	const result = await getUserByName(name)
+	const user = result[0]
+	if (!user) {
+		const error = new Error(errorType.USER_DOES_NOT_EXISTS)
+		return ctx.app.emit('error', error, ctx)
+	}
+	// 4. 判断密码是否和数据库中的密码是否一致
+	if (md5password(password) !== user.password) {
+		const error = new Error(errorType.PASSWORD_IS_INCORRENT)
+		return ctx.app.emit('error', error, ctx)
+	}
+	await next()
+}
+
+module.exports = { verifyLogin }
